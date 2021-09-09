a8e53f133742464d55fb6fad0082af6ad1c5e623
Manoj-36 <n1k0l4t35l4369@gmail.com>
2021-09-09 19:49:17 +0530

    updates on loading page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Search from "./Components/search";
 import ReadDb from "./Components/readdb";
@@ -9,32 +9,47 @@ import Home from "./Components/Home";
 import Colleges from "./Components/Colleges";
 import Request from "./Components/Request";
 import Todolist from "./Components/Todolist";
+import Cimg from "./Components/images/clg.png";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
   return (
     <div>
-      <Router>
-        <Switch>
-          <Route exact path="/collage">
-            <Home />
-          </Route>
-        </Switch>
-        <Switch>
-          <Route exact path="/collage/Colleges">
-          <Colleges />
-          </Route>
-        </Switch>
-        <Switch>
-          <Route exact path="/collage/ReadDb">
-          <ReadDb />
-          </Route>
-        </Switch>
-        <Switch>
-          <Route exact path="/collage/Request">
-          <Request />
-          </Route>
-        </Switch>
-      </Router>
+      {loading ? (
+        <div className="load-icon">
+          <img className="load-img" src={Cimg} />
+        </div>
+      ) : (
+        <Router>
+          <Switch>
+            <Route exact path="/collage">
+              <Home />
+            </Route>
+          </Switch>
+          <Switch>
+            <Route exact path="/collage/Colleges">
+              <Colleges />
+            </Route>
+          </Switch>
+          <Switch>
+            <Route exact path="/collage/ReadDb">
+              <ReadDb />
+            </Route>
+          </Switch>
+          <Switch>
+            <Route exact path="/collage/Request">
+              <Request />
+            </Route>
+          </Switch>
+        </Router>
+      )}
     </div>
   );
 }
